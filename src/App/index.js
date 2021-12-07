5ff24df29cb71e65f76e74fec72ef118fa88e1bc
unknown <dark.desires@live.cl>
2021-12-07 14:14:14 -0600

    Component composition Lv1

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,65 @@
 import React from "react";
-import { AppUI  } from "./AppUI";
-import {TodoProvider} from '../TodoContext';
+//import { AppUI  } from "./AppUI";
+//import {TodoProvider} from '../TodoContext';
+
 
 function App() {
-
+  const [state, setState] = React.useState('Shared state');
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-    
-
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter/>
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state}/>
+      </TodoList>
+    </React.Fragment>
+     
+   
   );
 }
+
+function TodoHeader( {children}) {
+  return (
+    <header>
+      {children}
+    </header>
+   
+  
+  );
+}
+
+function TodoList( {children }) {
+  return (
+    <section className="TodoList-container">
+      {children}
+    </section>
+  );
+}
+
+function TodoCounter({props}) {
+  return <p>Todo Counter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+
+function TodoItem({state}) {
+  return <p>TodoItem: {state}</p>;
+}
+
+
+// function App() {
+
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+    
+
+//   );
+// }
 
 export default App;

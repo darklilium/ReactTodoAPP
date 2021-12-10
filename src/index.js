ed572b58a5d6f31bbe9c6f946b44dc682344e06e
unknown <dark.desires@live.cl>
2021-12-10 15:12:56 -0600

    Simple case with high order fx and components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index';
+//import App from './App/index';
 
+function App({greetType, name}) {
+    return (
+        <h1>!{greetType} {name}!</h1>
+    )
+}
+
+
+function withWhatever(WrappedComponent) { //any other high order fx
+    return function TrulyComponent(props) {  //always a react component (always the last return)
+        return (
+            <React.Fragment>
+                <WrappedComponent {...props}/>                   
+                <p>We are here to get along with Wrapper comp.</p>
+            </React.Fragment>
+        )
+    }
+}
+
+const AppWithWhatever = withWhatever(App);
 
 ReactDOM.render(
-    <App/>,
+    // <App greetType='buenas' name='eve'/>,
+    <AppWithWhatever greetType='buenas' name='eve'/>,
     document.getElementById('myApp')
 );
 

81568633deab66ee65b6258668fe81a3dff097a1
unknown <dark.desires@live.cl>
2021-12-10 15:20:10 -0600

    High order components example

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ import ReactDOM from 'react-dom';
 function App({greetType, name}) {
     return (
         <h1>!{greetType} {name}!</h1>
-    )
+    );
 }
 
 
-function withWhatever(WrappedComponent) { //any other high order fx
-    return function TrulyComponent(props) {  //always a react component (always the last return)
-        return (
-            <React.Fragment>
-                <WrappedComponent {...props}/>                   
-                <p>We are here to get along with Wrapper comp.</p>
-            </React.Fragment>
-        )
+function withGreeting(WrappedComponent) { //any other high order fx
+    return function WrapperComponentWithGreeting(greetType) {
+        return function TrulyComponent(props) {  //always a react component (always the last return)
+            return (
+                <React.Fragment>
+                    <WrappedComponent {...props} greetType={greetType}/>                   
+                    <p>We are here to get along with Wrapper comp.</p>
+                </React.Fragment>
+            )
+        }
     }
 }
 
-const AppWithWhatever = withWhatever(App);
+const AppWithGreeting = withGreeting(App)('Heyaa');
 
 ReactDOM.render(
     // <App greetType='buenas' name='eve'/>,
-    <AppWithWhatever greetType='buenas' name='eve'/>,
+    <AppWithGreeting name='eve'/>,
     document.getElementById('myApp')
 );
 
